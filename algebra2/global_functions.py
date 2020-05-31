51d5e44a7a2f8c79e871385e042f77f4d6bcfcfb
Christian <iifrostii@outlook.com>
2020-05-31 19:32:13 -0400

    add format_exponents() method

--- algebra2/global_functions.py
+++ algebra2/global_functions.py
@@ -4,3 +4,6 @@ from sys import platform
 def copy(answer): # copy to clipboard
     cmd = ('echo "{0}"|clip'.format(answer.strip())) if platform == "Windows" else ('echo "{0}"|pbcopy'.format(answer.strip()))
     return check_call(cmd, shell=True)
+
+def format_exponents(answer):  # use ^ instead of * to make the solution easier to read
+    return answer.replace("**", "^")
